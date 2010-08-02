7058d3db5aff343f0ddaf2ded140bb03fd2011e5
Kenshinakh <kenshin.akha@gmail.com>
2010-08-02 14:45:16 -0700

    8/2/2010

---
+++ 1001_world_fixes.sql
@@ -0,0 +1,16 @@
+UPDATE creature_template SET faction_A = 2095, faction_H = 2095 WHERE entry = 28821;
+UPDATE creature SET position_x = 2368.87 , position_y = -5778.70, position_z = 151.41 WHERE guid = 128739;
+UPDATE creature_template SET faction_A = 14, faction_H = 14, difficulty_entry_1 = 0 WHERE entry = 29573;
+UPDATE creature SET position_x = 588.49, position_y = -180.56, position_z = 396.91 WHERE guid = 137611;
+UPDATE creature_template SET faction_A = 21, faction_H = 21 WHERE entry = 34728;
+UPDATE creature_template SET faction_A = 21, faction_H = 21 WHERE entry = 34734;
+UPDATE creature_template SET faction_A = 21, faction_H = 21 WHERE entry = 34808;
+UPDATE creature_template SET faction_A = 14, faction_H = 14 WHERE entry = 34838;
+UPDATE creature_template SET faction_A = 14, faction_H = 14 WHERE entry = 34839;
+UPDATE creature_template SET faction_A = 14, faction_H = 14 WHERE entry = 34965;
+UPDATE creature_template SET faction_A = 14, faction_H = 14, difficulty_entry_1 = 0, mindmg = 383, maxdmg = 500, attackpower = 500, dmg_multiplier = 7 WHERE entry = 27984;
+UPDATE creature_template SET faction_A = 14, faction_H = 14, difficulty_entry_1 = 0, mindmg = 383, maxdmg = 500, attackpower = 500, dmg_multiplier = 7 WHERE entry = 27983;
+UPDATE creature_template SET faction_A = 14, faction_H = 14, difficulty_entry_1 = 0, mindmg = 383, maxdmg = 500, attackpower = 500, dmg_multiplier = 7 WHERE entry = 27985;
+UPDATE creature_template SET faction_A = 14, faction_H = 14, difficulty_entry_1 = 0, mindmg = 383, maxdmg = 500, attackpower = 500, dmg_multiplier = 7 WHERE entry = 27979;
+UPDATE creature_template SET faction_A = 14, faction_H = 14, difficulty_entry_1 = 0, mindmg = 383, maxdmg = 500, attackpower = 500, dmg_multiplier = 7 WHERE entry = 27982;
+
